4280e4877ef554ee494ea8f8bbb4016cb165d39a
eucandre <eucandre@gmail.com>
2017-07-06 11:53:29 -0300

    atualiza o index

--- index.php
+++ index.php
@@ -31,22 +31,39 @@
         <div id="container">
 
 
-            <div id="quadroConteudo">
+            <div id="quadroConteudo" style=" background: url(img/capa-anuário-online2.1.png) right no-repeat; background-size: 1000px;"> 
                 <div id="menu">
-                    <?php include 'menu.php'; ?>
+                    <?php include 'menu.php'; ?> 
                 </div>
 
                 <div id="conteudo">
                     <div id="texto">
-                        <h3>Apresentação</h3>
+                        <center><h3><b>APRESENTAÇÃO</b></h3></center>
 
                         <br />
-                        <p>O Anuário Estatístico do Estado do Paraná 2014 contém informações de dados estatísticos sobre a realidade estadual, as quais permitem conhecer e analisar o comportamento socioeconômico e ambiental do Estado e de seus 399 municí­pios.</p>
-                        <p>Os dados estão agrupados em cinco grandes Áreas: território, infraestrutura, características demográficas e sociais da população, economia e indicadores.</p>
-                        <p>Vale esclarecer que os dados de produção agropecuária provenientes do IBGE (PAM, PPM e PEVS) não foram atualizados em razão de alteração na data de divulgação dos mesmos.</p>
-                        <p>O IPARDES agradece a todas as pessoas, instituições e entidades que contribuíram para a realização deste trabalho principalmente como fontes de informações, e coloca-se à  disposição dos usuários para sugestões e crí­ticas, o que permitirá¡ aperfeiçoá-lo.</p>
+                        <p><i>
+                        A produção de informações, embasadas em dados estatísticos, constitui um subsídio indispensável ao processo de tomada de decisão e tem contribuído sistematicamente para o avanço do desenvolvimento econômico e social do Estado de Alagoas. Nesse sentido, a Secretaria de Estado do Planejamento, Gestão e Patrimônio – Seplag, por meio da Superintendência de Produção da Informação e do Conhecimento, apresenta a 23ª edição do <b> Anuário Estatístico do Estado de Alagoas</b>, referente ao exercício 2016.
+                        </p></i>
+                        <p><i>
+                        Trata-se de um trabalho que, dividido em cinco partes, reúne informações sobre o Estado de Alagoas e seus municípios, contemplando aspectos diversos nas áreas de geografia, demografia, educação, saúde, saneamento, segurança pública, movimento eleitoral, mercado de trabalho, atividades agropecuárias, atividades industriais, serviços, finanças, comércio de bens, instituições financeiras e sistema de contas. 
+                        </p></i>
+                        <p><i>
+                        Este documento é resultado do empenho dos profissionais que compõem o corpo técnico da Seplag e da contribuição de diversas instituições, sobretudo no que concerne à cessão de informações fundamentais à elaboração de um trabalho deste porte.
+                        </p></i>
+                        <p><i>
+                        Com o <b>Anuário Estatístico do Estado de Alagoas</b>, reafirmamos o compromisso de organizar, tratar e disseminar informações sobre a realidade do Estado, contribuindo, assim, para seu desenvolvimento. As Informações produzidas pela Seplag, permanentemente atualizadas, podem ser encontradas em nosso portal, pelo endereço: <a href="http://www.dados.al.gov.br/"> www.dados.al.gov.br</a> .
+                        </p></i>
 
-                        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+<br />
+
+                        <p><center>
+                            <b>FABRÍCIO MARQUES SANTOS</b> <br />
+                            <i>Secretário de Estado do Planejamento, Gestão e Patrimônio</i>
+
+                            </center>
+                        </p>
+
+                        <br /><br /><br /><br /><br />
                     </div>
                 </div>
                 <div style="clear: both;"></div>
