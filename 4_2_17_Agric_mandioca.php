b47a34cf1589120106df82687a06f11d36d668f4
eucandre <eucandre@gmail.com>
2017-07-07 13:05:18 -0300

    ajustes para a codificação do anuário

--- 4_2_17_Agric_mandioca.php
+++ 4_2_17_Agric_mandioca.php
@@ -3,7 +3,8 @@
     <head>
         <link rel="stylesheet" type="text/css" href="style.css">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta charset="utf-8"/>
         <link rel="stylesheet" href="bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <script src="bootstrap.min.js"></script>
--- head1.php
+++ head1.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
 <html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta charset="utf-8"/>
+</head>
  Anuário Estatístico de Alagoas 
  <a href="http://www.dados.al.gov.br"><img src='img/logo3.png' style="height:50px; float:right" title="Visualizar"/></a><br />
 </html>
